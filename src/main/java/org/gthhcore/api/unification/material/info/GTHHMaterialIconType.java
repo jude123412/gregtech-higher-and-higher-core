24c65b85dd2f5abe409278d201cf02dff892e78d
jude123412 <104434396+jude123412@users.noreply.github.com>
2026-01-19 08:29:55 +0000

    Ore DIctionary + En_Us Entries removal

--- src/main/java/org/gthhcore/api/unification/material/info/GTHHMaterialIconType.java
+++ src/main/java/org/gthhcore/api/unification/material/info/GTHHMaterialIconType.java
@@ -9,11 +9,5 @@ public class GTHHMaterialIconType {
     public static final MaterialIconType fragment = new MaterialIconType("fragment");
     public static final MaterialIconType murky = new MaterialIconType("murky");
     public static final MaterialIconType raw = new MaterialIconType("raw");
-    public static final MaterialIconType rodFuelSingle = new MaterialIconType("rodFuelSingle");
-    public static final MaterialIconType rodFuelDual = new MaterialIconType("rodFuelDual");
-    public static final MaterialIconType rodFuelQuad = new MaterialIconType("rodFuelQuad");
-    public static final MaterialIconType rodDepletedSingle = new MaterialIconType("rodDepletedSingle");
-    public static final MaterialIconType rodDepletedDual = new MaterialIconType("rodDepletedDual");
-    public static final MaterialIconType rodDepletedQuad = new MaterialIconType("rodDepletedQuad");
     public static final MaterialIconType compressed = new MaterialIconType("compressed");
 }
